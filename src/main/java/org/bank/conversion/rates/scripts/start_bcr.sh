9304cdb09b9e72e8a8c11346c64f8d544edebe4c
Branislav Malo <branislavmalo@gmail.com>
2024-01-12 08:14:25 +0100

    Add start up script
    Signed-off-by: Branislav Malo <branislavmalo@gmail.com>

--- src/main/java/org/bank/conversion/rates/scripts/start_bcr.sh
+++ src/main/java/org/bank/conversion/rates/scripts/start_bcr.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
+# Initialize variables
+USER_PATH="$HOME/BankConversionRates"
+
+# Parse command line options
+while getopts "p:" opt; do
+  case $opt in
+    p)
+      USER_PATH="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+# Check if the user provided the -p option
+if [ "$OPTIND" -eq 1 ]; then
+  echo "No path specified to the BankConversionRates."
+  exit 1
+fi
+
 # Set the path to your JAR file
-JAR_FILE="path/to/your/BankConversionRates-0.0.1-SNAPSHOT.jar"
+JAR_FILE="$USER_PATH/BankConversionRates/target/BankConversionRates-0.0.1-SNAPSHOT.jar"
 
 # Check if the JAR file exists
 if [ ! -f "$JAR_FILE" ]; then
